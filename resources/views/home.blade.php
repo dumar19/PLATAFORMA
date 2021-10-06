9d0ab47b602cfc7d4865dda2f39b28d8c1a986b6
unknown <dumar.canizales@gmail.com>
2021-10-06 09:51:18 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layaouts.plantilla')
+
+
+@section('title', 'home')
+
+@section('content');
+<h1>Bienvenido a la pagina principal;</h1>
+
+@endsection();
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!--favicon-->
+    <!--estilos-->
+</head>
+<body>
+    <!--header-->
+    <!--nav-->
+    <h1>
+    
+    </h1>
+    <!--footer-->
+    <!--script-->
+
+    
+</body>
+</html>
